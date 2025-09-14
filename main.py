cf418d2b59f065e65e615fe23bd177184f315f89
javier galvan <javieral256568@gmail.com>
2025-09-14 10:48:50 -0700

    read content-type

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ class Item(BaseModel):
     description: str = None
 
 @app.post("/items/")
-async def create_item(item: Item):
-    return {"name": item.name, "description": item.description}
+async def create_item(item: Item, request: Request):
+    content_type = request.headers.get("content-type")
+    return {
+        "name": item.name,
+        "description": item.description,
+        "content_type": content_type
+    }
